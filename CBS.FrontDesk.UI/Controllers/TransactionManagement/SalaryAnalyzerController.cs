9abdc5e6a8376c6fc8f89e27a5a2c3120044caf7
BIOSv13 <enzyrio@yahoo.com>
2025-02-03 13:01:04 +0100

    Operations

--- CBS.FrontDesk.UI/Controllers/TransactionManagement/SalaryAnalyzerController.cs
+++ CBS.FrontDesk.UI/Controllers/TransactionManagement/SalaryAnalyzerController.cs
@@ -37,7 +37,7 @@ namespace CBS.FrontDesk.UI.Controllers.TransactionManagement
         {
             return View(new SalaryUploadModelCarrier());
         }
-        public async Task<ActionResult> Salary(string fileUploadid)
+        public async Task<ActionResult> Detail(string fileUploadid)
         {
             var salaryUploadModels = await _salaryUploadServices.GetSalaryModelByBranchUsingFileId(fileUploadid);
             var fileUpload = await _salaryUploadServices.GetFileUpload(fileUploadid);
