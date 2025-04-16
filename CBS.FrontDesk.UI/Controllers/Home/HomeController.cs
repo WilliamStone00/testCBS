3bd1777b8174e046ff2a658ef92bcd4a2b6e767a
BIOSv13 <enzyrio@yahoo.com>
2025-04-16 18:35:24 +0100

    Language integration

--- CBS.FrontDesk.UI/Controllers/Home/HomeController.cs
+++ CBS.FrontDesk.UI/Controllers/Home/HomeController.cs
@@ -19,14 +19,14 @@ namespace CBS.FrontDesk.UI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = Resources.GlobalAbout.AboutInformation;
+            
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = Resources.GlobalAbout.AboutInformation;
 
             return View();
         }
---
+++ CBS.FrontDesk.UI/Controllers/Localization/LocalizationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBS.FrontDesk.UI.Controllers.Language
+{
+    public class LocalizationController : Controller
+    {
+        public ActionResult SetLanguage(string lang)
+        {
+            if (!string.IsNullOrEmpty(lang))
+            {
+                // Set session for UI logic
+                Session["SelectedLanguage"] = lang;
+
+                // Set cookie for request culture
+                HttpCookie cookie = new HttpCookie("TSC_Lang", lang)
+                {
+                    Expires = DateTime.Now.AddYears(1)
+                };
+                Response.Cookies.Add(cookie);
+            }
+
+            string returnUrl = Request.UrlReferrer?.ToString() ?? "/";
+            return Redirect(returnUrl);
+        }
+    }
+
+}
