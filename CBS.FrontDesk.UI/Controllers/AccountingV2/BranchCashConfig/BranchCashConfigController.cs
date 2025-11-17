8ef0ee333d10796026b7b0c9892600aa84c81cc1
Awath Eugene <enzyrio@yahoo.com>
2025-11-17 13:38:42 +0100

    Product and Branch Cash Config OK

--- CBS.FrontDesk.UI/Controllers/AccountingV2/BranchCashConfig/BranchCashConfigController.cs
+++ CBS.FrontDesk.UI/Controllers/AccountingV2/BranchCashConfig/BranchCashConfigController.cs
@@ -240,20 +240,37 @@ namespace CBS.FrontDesk.UI.Controllers.AccountingManagement
                         {
                             id = config.Id,
                             branchId = config.BranchId,
+
+                            // Core cash-control (mandatory)
                             cashInHandAccountId = config.CashInHandAccountId,
                             vaultAccountId = config.VaultAccountId,
                             surplusIncomeAccountId = config.SurplusIncomeAccountId,
-                            revenueAccountId = config.RevenueAccountId,
                             shortageExpenseAccountId = config.ShortageExpenseAccountId,
+
+                            // Generic / fallback revenue & partners
+                            revenueAccountId = config.RevenueAccountId,
                             partnerAccountId = config.PartnerAccountId,
                             camcculAccountId = config.CamcculAccountId,
                             headOfficeLiaisonAccountId = config.HeadOfficeLiaisonAccountId,
                             formFeeIncomeAccountId = config.FormFeeIncomeAccountId,
+
+                            // Loan transit
+                            loanTransitAccountId = config.LoanTransitAccountId,
+
+                            // 🔹 New fallback / backup accounts
+                            cashInCommisionAccountIDAccountId = config.CashInCommisionAccountIDAccountId,
+                            cashOutCommisionAccountIDAccountId = config.CashOutCommisionAccountIDAccountId,
+                            transfterCommisionAccountIDAccountId = config.TransfterCommisionAccountIDAccountId,
+                            vatAccountId = config.VATAccountId,
+                            interestGeneratedFromAccountId = config.InterestGeneratedFromAccountId,
+
+                            // Other flags
                             realTimeCashPosting = config.RealTimeCashPosting
                         },
                         // send as simple array of {text, value} for the client
                         accounts = accounts
                     });
+
                 }
 
                 // New config for this branch
@@ -328,8 +345,28 @@ namespace CBS.FrontDesk.UI.Controllers.AccountingManagement
                     FormFeeIncomeAccountName = NameNo(c.FormFeeIncomeAccountId).name,
                     FormFeeIncomeAccountNumber = NameNo(c.FormFeeIncomeAccountId).number,
 
+                    LoanTransitAccountName = NameNo(c.LoanTransitAccountId).name,
+                    LoanTransitAccountNumber = NameNo(c.LoanTransitAccountId).number,
+
+                    // 🔹 New fallback / backup accounts
+                    CashInCommisionAccountName = NameNo(c.CashInCommisionAccountIDAccountId).name,
+                    CashInCommisionAccountNumber = NameNo(c.CashInCommisionAccountIDAccountId).number,
+
+                    CashOutCommisionAccountName = NameNo(c.CashOutCommisionAccountIDAccountId).name,
+                    CashOutCommisionAccountNumber = NameNo(c.CashOutCommisionAccountIDAccountId).number,
+
+                    TransfterCommisionAccountName = NameNo(c.TransfterCommisionAccountIDAccountId).name,
+                    TransfterCommisionAccountNumber = NameNo(c.TransfterCommisionAccountIDAccountId).number,
+
+                    VATAccountName = NameNo(c.VATAccountId).name,
+                    VATAccountNumber = NameNo(c.VATAccountId).number,
+
+                    InterestGeneratedFromAccountName = NameNo(c.InterestGeneratedFromAccountId).name,
+                    InterestGeneratedFromAccountNumber = NameNo(c.InterestGeneratedFromAccountId).number,
+
                     RealTimeCashPosting = c.RealTimeCashPosting
                 };
+
             }).ToList();
 
             return Json(new { data = rows });
