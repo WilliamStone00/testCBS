26987f0e045f50a955b91a2efacabc63eae1edb4
simone4512 <pridesimone@yahoo.com>
2025-11-21 22:16:44 +0100

    updated

---
+++ CBS.BusinessService/CBS.BusinessService/AccountingV2/GLSystemReconciliation/CloseOfDayModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CBS.BusinessService.AccountingV2.GLSystemReconciliation
+{
+    public class CloseOfDayModel
+    {
+
+        public string BranchId { get; set; }
+        public string Reference { get; set; }
+        public bool AllTransactionsReconciled { get; set; }
+        public bool CashVaultVerified { get; set; }
+        public bool InterBranchConfirmed { get; set; }
+        public bool AnyIssueToBeRaised { get; set; }
+        public string Comment { get; set; }
+        public string ClosedBy { get; set; }
+    }
+}
