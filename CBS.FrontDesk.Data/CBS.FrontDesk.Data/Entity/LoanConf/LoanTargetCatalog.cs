c9126f426125a0a27c453ecc43a4605a80642da3
Your Name <collinssangong900@gmail.com>
2025-12-26 05:46:39 +0100

    update

---
+++ CBS.FrontDesk.Data/CBS.FrontDesk.Data/Entity/LoanConf/LoanTargetCatalog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBS.FrontDesk.Data.Entity.LoanConf
+{
+   
+        public class LoanTargetCatalog
+        {
+            public string Id { get; set; }
+
+      
+            public string NameEn { get; set; }
+
+            public string NameFr { get; set; }
+
+           public int PcmfPopulation { get; set; }
+
+            public bool IsActive { get; set; }
+
+            public DateTime? CreatedDate { get; set; }
+            public string CreatedBy { get; set; }
+            public DateTime? ModifiedDate { get; set; }
+            public string ModifiedBy { get; set; }
+        }
+
+        public class CreateLoanTargetCatalogRequest
+        {
+            
+            public string NameEn { get; set; }
+
+        
+            public string NameFr { get; set; }
+
+            public int PcmfPopulation { get; set; }
+
+            public bool IsActive { get; set; }
+        }
+
+        public class UpdateLoanTargetCatalogRequest
+        {
+          
+            public string Id { get; set; }
+
+         
+            public string NameEn { get; set; }
+
+            public string NameFr { get; set; }
+
+           public int PcmfPopulation { get; set; }
+
+            public bool IsActive { get; set; }
+        }
+
+        public class DeleteLoanTargetCatalogRequest
+        {
+              public string Id { get; set; }
+        }
+
+        public class LoanTargetCatalogQuery
+        {
+              public string NameFilter { get; set; }
+            public bool? IsActiveFilter { get; set; }
+        }
+    }
+
