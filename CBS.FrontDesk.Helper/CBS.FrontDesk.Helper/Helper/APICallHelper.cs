f22afd276ee2caf7bc5c57d7c9f80112845d3f3a
garveydel <garveypeka@gmail.com>
2026-01-08 12:58:57 +0100

    Adding CounterChequeDataTable View and Updated CounterChequeController

--- CBS.FrontDesk.Helper/CBS.FrontDesk.Helper/Helper/APICallHelper.cs
+++ CBS.FrontDesk.Helper/CBS.FrontDesk.Helper/Helper/APICallHelper.cs
@@ -1334,14 +1334,12 @@ namespace CBS.FrontDesk.Helper
 
         //------------------------ Counter Cheque ------------------------
         public const string IssueCounterCheque = "/api/v1/counter-checks";
-        public const string GetCounterChequesForDataTable = "/api/v1/counter-checks/data-table";
+        public const string GetCounterChequeDataTable = "/api/v1/counter-checks/data-table";
         public const string ReviewCounterCheque = "/api/v1/counter-checks/{0}/review";
         public const string ValidateCounterCheque = "/api/v1/counter-checks/{0}/validate";
         public const string RejectCounterCheque = "/api/v1/counter-checks/{0}/reject";
 
         public const string GetCounterChequeDetails = "/api/v1/counter-checks";
-
-        public const string datatableforcounterrequest = "/api/v1/counter-checks/data-table";
 
         //----------------------- Cheque Book -------------------------
         public const string GetChequeBooksDataTable = "/api/v1/checkbook/data-table";
