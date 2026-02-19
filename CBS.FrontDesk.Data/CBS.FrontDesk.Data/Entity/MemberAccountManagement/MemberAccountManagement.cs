4b3ebdb80c5620c1edf02bc15618e716daf9df44
AGISANG <159634519+AGISANG@users.noreply.github.com>
2026-02-19 18:20:18 +0100

    update

--- CBS.FrontDesk.Data/CBS.FrontDesk.Data/Entity/MemberAccountManagement/MemberAccountManagement.cs
+++ CBS.FrontDesk.Data/CBS.FrontDesk.Data/Entity/MemberAccountManagement/MemberAccountManagement.cs
@@ -27,7 +27,7 @@ namespace CBS.FrontDesk.Data.Entity.MemberAccountManagement
         public string Status { get; set; }
 
         public string RequestedByName { get; set; }
-        public DateTimeOffset? RequestedOn { get; set; }
+        public DateTime? RequestedOn { get; set; }
 
         public string DecidedByName { get; set; }
         public DateTime? DecidedOn { get; set; }
@@ -47,44 +47,10 @@ namespace CBS.FrontDesk.Data.Entity.MemberAccountManagement
     }
     public class Decission
     {
-        public string Id { get; set; }
-
-        public string AccountId { get; set; }
-        public string AccountNumber { get; set; }
-        public string AccountType { get; set; }
-
-        public string MemberName { get; set; }
-        public string MemberReference { get; set; }
-
-        public string BranchId { get; set; }
-        public string BranchName { get; set; }
-        public string BranchCode { get; set; }
-
-        public AccountManagementAction Action { get; set; }
-        public string Status { get; set; }
-
-        public string RequestedByName { get; set; }
-        public DateTime? RequestedOn { get; set; }
-
-        public string DecidedByName { get; set; }
-        public DateTime? DecidedOn { get; set; }
-
-        public DateTime? AppliedOn { get; set; }
-        public DateTime? EffectiveDate { get; set; }
-
-        public string RequestReason { get; set; }
-        public string DecisionNote { get; set; }
-        public string BlockReason { get; set; }
-
-        public decimal Amount { get; set; }
-
-        public string ApplyError { get; set; }
-        public string TraceLog { get; set; }
-        public string CorrelationId { get; set; }
-
         public string WorkflowId { get; set; }
         public string Decision { get; set; }
-        //public string DecisionNote { get; set; }      
+        public string DecisionNote { get; set; }
+
     }
 
     public enum AccountManagementAction
