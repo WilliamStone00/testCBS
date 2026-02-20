4a826ca68129cc178068eb5e3e21dc2ff8e68537
CodeVoyagerX-cyber <Royfomum@gmail.com>
2026-02-20 13:34:07 +0100

    update cheque

--- CBS.FrontDesk.Data/CBS.FrontDesk.Data/Entity/CheckManagementSystem/Clearance/ClearanceRequest/OptionRequest.cs
+++ CBS.FrontDesk.Data/CBS.FrontDesk.Data/Entity/CheckManagementSystem/Clearance/ClearanceRequest/OptionRequest.cs
@@ -34,7 +34,7 @@ namespace CBS.FrontDesk.Data.Entity.CheckManagementSystem.Clearance.ClearanceReq
         public string ExternalChequeNumber { get; set; }
         public string ExternalBankName { get; set; }
         
-        public int ChequeType { get; set; }
+        public bool ChequeType { get; set; }
   
 
         public string ExternalBankCode { get; set; }      // Code banque (SWIFT / BIC)
@@ -98,7 +98,7 @@ namespace CBS.FrontDesk.Data.Entity.CheckManagementSystem.Clearance.ClearanceReq
         public string CustomerId { get; set; }
 
         public string CheckBookNumber { get; set; } = null;
-        public DateTime CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
 
         public int? CheckBookPageNumber { get; set; }
         public string AccountNumber { get; set; }
@@ -144,7 +144,7 @@ namespace CBS.FrontDesk.Data.Entity.CheckManagementSystem.Clearance.ClearanceReq
         public string ClearedBy { get; set; }
         public string RejectedBy { get; set; }
 
-        public DateTime SubmissionDate { get; set; }
+        public DateTime? SubmissionDate { get; set; }
         public DateTime? ProcessedDate { get; set; }
         public DateTime? ClearedDate { get; set; }
         public DateTime? RejectionDate { get; set; }
@@ -153,7 +153,7 @@ namespace CBS.FrontDesk.Data.Entity.CheckManagementSystem.Clearance.ClearanceReq
         public decimal DeduceAmount { get; set; }
         public decimal ReceiveAmount { get; set; }
         public decimal Charges { get; set; }
-
+      
         
 
         public string CheckBook { get; set; }
